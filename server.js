88aea08d6dcbb209bbaa52916c80f74cdbb4cc80
tahnoun <ts2010@hw.ac.uk>
2020-11-23 20:12:37 +0400

    updated server.js

--- server.js
+++ server.js
@@ -20,6 +20,42 @@ app.get('/', function(request, response) {
 app.use(express.json());
 app.use(express.urlencoded());
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database:"game"
+});
+
+function executeQuery(query, callback) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            return callback(err, null);
+        } else if (connection) {
+            connection.query(query, function(err, rows, fields) {
+                connection.release();
+                if (err) {
+                    return callback(err, null);
+                }
+                return callback(null, rows);
+            });
+        } else {
+            return callback(true, "No Connection");
+        }
+    });
+}
+
+
+function getResult(query, callback) {
+    executeQuery(query, function(err, rows) {
+        if (!err) {
+            callback(null, rows);
+        } else {
+            callback(true, err);
+        }
+    });
+}
+
 //setting up port
 const port = process.env.PORT || 3000;
 const server = app.listen(port, function() {
@@ -33,8 +69,36 @@ const gameRouter = require('./game_routes/post');
 app.use(gameRouter);
 
 //create database if not exists
-const createDB = require('./database/db');
-createDB();
+
+connection.connect(function(err) {
+    if (err) throw err;
+    //create database
+    const sqlDB = "CREATE DATABASE IF NOT EXISTS `leaderboard`;";
+    connection.query(sqlDB, function(err, result) {
+        if (err) throw err;
+        console.log('The database has been created');
+    });
+    //change database
+    connection.changeUser({ database: 'leaderboard' }, function(err) {
+        if (err) {
+            console.log('error in changing database', err);
+            return;
+        }
+    });
+
+    //create table Players
+    const sqlUser = "Create table if not exists `leaderboard`.`users`(" +
+        "id int(10) " +
+        "`pseudoname` varchar(32) NOT NULL default 'Unkown'," + +
+        "`score` int(10) , "+
+        "PRIMARY KEY (`id`)" +
+        "); ";
+    connection.query(sqlUser, function(err, result) {
+        if (err) throw err;
+        console.log("Users table created");
+    });
+     
+});
 
 //loading socket.io and binding to the server
 const io = require('socket.io')(server);
@@ -52,7 +116,7 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('gameOver',function(score){
-        
+
 
     });
 
